21f1abdb65da4c56136328e425cdd1bcac201b31
Enrique Sarabia Roque <54542442+EnriqueSaro@users.noreply.github.com>
2021-03-29 23:34:38 -0600

    Some tests added

--- unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -1,8 +1,28 @@
 package com.udacity.examples.Testing;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class HelperTest {
 
-	
+    @Test
+    public void verify_getMergedList(){
+
+        List<String> cadenas = Arrays.asList("Unir","estas","cadenas");
+        String mergedList = Helper.getMergedList(cadenas);
+
+        Assert.assertEquals("Unir, estas, cadenas",mergedList);
+    }
+    @Test
+    public void verify_getFilteredList(){
+
+        List<String> cadenas = Arrays.asList("Filtrar","","cadenas","");
+        List<String> filteredList = Helper.getFilteredList(cadenas);
+        List<String> filtro = Arrays.asList("Filtrar","cadenas");
+
+        Assert.assertEquals(filtro,filteredList);
+    }
 }
